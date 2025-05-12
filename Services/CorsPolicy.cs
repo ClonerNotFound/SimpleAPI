2af2bc6d09e789184acfa7e9582023bf42143b60
ClonerNotFound <clonernotfound@gmail.com>
2025-05-12 18:21:47 +0300

    добавил настройку пользовательских cors

---
+++ Services/CorsPolicy.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleAPI.Services
+{
+    public class CorsPolicy
+    {
+        public static void ConfigureCorsPolicy(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin", builder =>
+                {
+                    builder.WithOrigins("http://localhost:3000")
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
+                });
+            });
+        }
+    }
+}
